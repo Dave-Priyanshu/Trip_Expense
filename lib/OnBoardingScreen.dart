fe63fb0befed0b9af2167a46e845ba759d65372f
Dave-Priyanshu <ims30priyanshu@gmail.com>
2023-10-09 00:33:51 +0530

    Updated OnBoardingScreens with custom dialogBox for Login

--- lib/OnBoardingScreen.dart
+++ lib/OnBoardingScreen.dart
@@ -1,91 +1,97 @@
-import 'package:flutter/material.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-import 'package:trip_expense/Home_widget.dart';
-import 'package:trip_expense/intro_screens/intro_page_1.dart';
-import 'package:trip_expense/intro_screens/intro_page_2.dart';
-import 'package:trip_expense/pages.dart';
-import 'package:trip_expense/views/new_trips/location_view.dart';
+// import 'package:flutter/material.dart';
+// import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+// import 'package:trip_expense/Home_widget.dart';
+// import 'package:trip_expense/intro_screens/intro_page_1.dart';
+// import 'package:trip_expense/intro_screens/intro_page_2.dart';
+// import 'package:trip_expense/pages.dart';
+// import 'package:trip_expense/views/new_trips/location_view.dart';
+//
+// class OnBoardingScreen extends StatefulWidget {
+//
 
-class OnBoardingScreen extends StatefulWidget {
 
 
-  @override
-  _OnBoardingScreenState createState() => _OnBoardingScreenState();
-}
+//   ********* old design************
 
-class _OnBoardingScreenState extends State<OnBoardingScreen> {
-  //controller to keep track on which page we're on
-  PageController _controller = PageController();
 
-  //keep track of if we are on the last page or not
-  bool onLastPage = false;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          //page view
-          PageView(
-            controller: _controller,
-            onPageChanged: (index){
-              setState(() {
-                onLastPage =(index ==1);
-              });
-            },
-            children: [
-              IntroPage1(),
-              IntroPage2()
-            ],
-          ), // <-- Add a comma here
-
-          //dot indicator
-          Container(
-            alignment: Alignment(0, 0.75),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-
-                //skip
-                GestureDetector(
-                  onTap: () {
-                    _controller.jumpToPage(1);
-                  },
-                    child: Text('Skip'),
-                ),
-      
-                //dot indicator
-                SmoothPageIndicator(controller: _controller, count: 2),
-
-                //next or done
-                onLastPage?
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(context,
-                    MaterialPageRoute(
-                        builder:(context){
-                          return Home();
-                        },
-                     ),
-                    );
-                  },
-                  child:  Text('Done'),
-                )
-                : GestureDetector(
-                  onTap: () {
-                    _controller.nextPage(
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.easeIn,
-                    );
-                  },
-                  child:  Text('Next'),
-                )
-
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   _OnBoardingScreenState createState() => _OnBoardingScreenState();
+// }
+//
+// class _OnBoardingScreenState extends State<OnBoardingScreen> {
+//   //controller to keep track on which page we're on
+//   PageController _controller = PageController();
+//
+//   //keep track of if we are on the last page or not
+//   bool onLastPage = false;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Stack(
+//         children: [
+//           //page view
+//           PageView(
+//             controller: _controller,
+//             onPageChanged: (index){
+//               setState(() {
+//                 onLastPage =(index ==1);
+//               });
+//             },
+//             children: [
+//               IntroPage1(),
+//               IntroPage2()
+//             ],
+//           ), // <-- Add a comma here
+//
+//           //dot indicator
+//           Container(
+//             alignment: Alignment(0, 0.75),
+//             child: Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               children: [
+//
+//                 //skip
+//                 GestureDetector(
+//                   onTap: () {
+//                     _controller.jumpToPage(1);
+//                   },
+//                     child: Text('Skip'),
+//                 ),
+//
+//                 //dot indicator
+//                 SmoothPageIndicator(controller: _controller, count: 2),
+//
+//                 //next or done
+//                 onLastPage?
+//                 GestureDetector(
+//                   onTap: () {
+//                     Navigator.push(context,
+//                     MaterialPageRoute(
+//                         builder:(context){
+//                           return Home();
+//                         },
+//                      ),
+//                     );
+//                   },
+//                   child:  Text('Done'),
+//                 )
+//                 : GestureDetector(
+//                   onTap: () {
+//                     _controller.nextPage(
+//                       duration: Duration(milliseconds: 500),
+//                       curve: Curves.easeIn,
+//                     );
+//                   },
+//                   child:  Text('Next'),
+//                 )
+//
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:trip_expense/Home_widget.dart';
-import 'package:trip_expense/OnBoardingScreen.dart';
-
-import 'OnBoardingScreen.dart';
+import 'package:trip_expense/views/first_view.dart';
+// import 'package:trip_expense/OnBoardingScreen.dart';
+//
+// import 'OnBoardingScreen.dart';
 // Future<void> main() async {
 //   WidgetsFlutterBinding.ensureInitialized();
 //   await Firebase.initializeApp();
@@ -23,7 +24,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: OnBoardingScreen(),
+      home: FirstView(),
+      routes: <String,WidgetBuilder>{
+        //this is route which will take us somewhere on the click of a button
+        '/signup': (BuildContext context) => Home(),
+        '/home': (BuildContext context) => Home(),
+      }
     );
   }
 }
